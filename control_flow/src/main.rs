ee175587fbacb19607ec0d4b3ed62788496f3045
Virak Khun <virak.khun@okoone.com>
2024-01-18 20:46:28 +0700

    control flow

---
+++ control_flow/src/main.rs
@@ -0,0 +1,54 @@
+fn main() {
+    cf_if();
+    cf_if_else();
+    cf_with_let();
+    cf_loop_with_return_value();
+}
+
+fn cf_if() {
+    let i = 20;
+
+    if i > 10 {
+        println!("condition is true")
+    } else {
+        println!("condition is false")
+    }
+}
+
+fn cf_if_else() {
+    let number = 6;
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+}
+
+fn cf_with_let() -> String {
+    let str = if 9 > 10 {
+        String::from("Not really")
+    } else {
+        String::from("Yes, but not really")
+    };
+
+    str
+}
+
+fn cf_loop_with_return_value() -> u32 {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    result
+}
